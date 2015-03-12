4b7f97a47e7999a698e54ee6f91bd54b7c3a52b4
icedream <icedream2k9@die-optimisten.net>
2015-03-12 10:02:48 +0100

    Normalize JWASM modules.

--- CMake/modules/CMakeDetermineASM_JWASMCompiler.cmake
+++ CMake/modules/CMakeDetermineASM_JWASMCompiler.cmake
@@ -1,26 +1,9 @@
+set(CMAKE_ASM_JWASM_COMPILER_INIT jwasm)
 
-#=============================================================================
-# Copyright 2001-2009 Kitware, Inc.
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
+if(NOT CMAKE_ASM_JWASM_COMPILER)
+	find_program(CMAKE_ASM_JWASM_COMPILER jwasm)
+endif(NOT CMAKE_ASM_JWASM_COMPILER)
 
-# Find the jwasm assembler.
-
-SET(CMAKE_ASM_JWASM_COMPILER_INIT jwasm)
-
-IF(NOT CMAKE_ASM_JWASM_COMPILER)
-  FIND_PROGRAM(CMAKE_ASM_JWASM_COMPILER jwasm)
-ENDIF(NOT CMAKE_ASM_JWASM_COMPILER)
-
-# Load the generic DetermineASM compiler file with the DIALECT set properly:
-SET(ASM_DIALECT "_JWASM")
-INCLUDE(CMakeDetermineASMCompiler)
-SET(ASM_DIALECT)
+set(ASM_DIALECT "_JWASM")
+include(CMakeDetermineASMCompiler)
+set(ASM_DIALECT)
